e3ded21f4e8cc084bbb49c1f9e39f562003e6c41
EDZ <chenglin.gao@plusx.cn>
2018-08-13 10:46:50 +0800

    ····

---
+++ src/components/header.vue
@@ -0,0 +1,84 @@
+<template>
+  <nav>
+    <div class="test" @click="showSide">
+      <span class="iconfont icon-menu" >
+      </span>
+    </div>
+    <router-link to="/mine"><img :src="avatarUrl" alt="" class="user"></router-link>
+    <router-link to="/my">my</router-link>
+    <router-link to="/home">home</router-link>
+    <router-link to="/search">search</router-link>
+    <!--<slideBar></slideBar>-->
+  </nav>
+
+</template>
+<script>
+  import {mapState,mapMutations, mapGetters,mapActions} from 'vuex'
+  // import {store} from "../store";
+  // import slideBar from '@/components/sideBar'
+   export default {
+     data() {
+       return {
+         avatarUrl: '../../static/img/user.png',
+       }
+     },
+     //计算属性
+     computed: {
+       //获取数据
+       ...mapGetters([   //获取 getters
+         'userData',
+         'isShowMethod'
+       ])
+     },
+     //监听,
+     watch: {
+       'userData': {
+         //添加方法
+         handler: 'changeData',   //绑定 handler方法
+         deep: true  //发现对象内部值的变化
+       },
+     },
+     methods: {
+       changeData() {
+         this.avatarUrl = this.userData.profile.avatarUrl
+       },
+       // showSide(){
+         //执行showSideBar方法
+         // this.dispatch('showSideBar')
+         // this.$store.dispatch('showSideBar')
+       // },
+     },
+     components: {
+       // 'slideBar':slideBar
+     },
+     //mounted 实时监听数据变换
+     mounted(){
+       console.log(this.isShow)
+       //如果不是第一次同样进行设置
+       if(!this.userData.first){
+         this.avatarUrl=this.userData.profile.avatarUrl
+       }
+     }
+   }
+</script>
+
+<style lang="scss" scoped>
+
+  nav {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 75px;
+    background: red;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    z-index: 99;
+  }
+  .user{
+    display: inline-block;
+    width: 50px;
+    height: 50px;
+  }
+</style>
---
+++ src/components/sideBar.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="home">
+    <transition name="fade" @touchmove.stop.prevent>
+      <div class="menu-mask" v-show="isRellyShow" @click="hideSide"></div>
+    </transition>
+    <transition name="slide-fade">
+      <div class="side-content" v-show="isRellyShow">
+        <a class="change">
+          <!--<img src="http://p4.qhimg.com/t0160e6a92121691e22.png" alt=""/>-->
+        </a>
+
+      </div>
+    </transition>
+  </div>
+
+</template>
+<script>
+  // import {store} from "../store";
+  export default {
+    data() {
+      return {}
+    },
+    methods: {
+      // hideSide: function () {
+      //   this.$store.dispatch('hideSideBar');
+      // },
+      computed: {
+        isRellyShow() {
+          // return this.$store.getters.isShowMethod;
+        }
+
+      },
+      components: {
+
+      },
+      mounted() {
+
+      },
+
+    }
+  }
+</script>
+
+<style lang="scss" type="text/scss" scoped>
+  .menu-mask {
+    position: fixed;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    opacity: 1;
+    z-index: 10;
+    background: rgba(0, 0, 0, 0.5);
+  }
+
+  .side-content {
+    z-index: 11;
+    position: fixed;
+    width: 286px;
+    height: 100%;
+    background: #e5e5e5;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    -webkit-overflow-scrolling: touch;
+  }
+
+  .fade-enter-to, .fade-leave-to {
+    transition: opacity 0.3s
+  }
+
+  .fade-enter, .fade-leave-to {
+    opacity: 0
+  }
+
+  .slide-fade-enter-to, .slide-fade-leave-to {
+    transition: transform 0.3s;
+    transform: translate(0px, 0px);
+  }
+
+  .slide-fade-enter, .slide-fade-leave-to {
+    opacity: 0;
+    -webkit-transform: translate(-286px, 0px);
+    transform: translate(-286px, 0px);
+    -webkit-transition: opacity 0.3s ease-in-out 0.3s, -webkit-transform 0.3s ease-in-out;
+    transition: opacity 0.3s ease-in-out 0.3s, transform 0.3s ease-in-out;
+  }
+
+  .change {
+    display: block;
+    width: 284px;
+    height: 200px;
+    background: url(http://p3.qhimg.com/t0134c65e59012a1257.png) no-repeat center;
+    background-size: cover;
+    border: 1px solid #fff;
+    overflow: hidden;
+  }
+
+  .change img {
+    display: block;
+    width: 220px;
+    height: 180px;
+    opacity: 0;
+    -webkit-transform: translate(284px, 200px);
+    transform: translate(284px, 200px);
+    -webkit-transition: opacity 0.5s ease-in-out 0.5s, -webkit-transform 1s ease-in-out;
+    transition: opacity 0.5s ease-in-out 0.5s, transform 1s ease-in-out;
+  }
+
+  .change:hover img {
+    -webkit-transform: translate(0px, 0px);
+    transform: translate(0px, 0px);
+    opacity: 1;
+  }
+</style>
