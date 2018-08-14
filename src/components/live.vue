58c9e521734d22c94f209ca7dbb7253cc45f0ccc
EDZ <chenglin.gao@plusx.cn>
2018-08-14 18:34:17 +0800

    live

---
+++ src/components/live.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="$app$"></div>
+</template>
+<script>
+    export default {
+        created() {
+
+        },
+        data() {
+            return {}
+        },
+        computed: {},
+        methods: {},
+        components: {},
+        mounted() {
+
+        }
+    }
+</script>
+
+<style lang="scss" type="text/scss" scoped>
+
+</style>
